f8b317f2e4e5108bd66b7e5a00a3e854e7904f39
Pankaj <Pankajgajjar@gmail.com>
2013-12-04 19:57:08 +0530

    Product Label with i18n support value

---
+++ Test Sample/PIMsamplePOC1.php
@@ -0,0 +1,97 @@
+<?php
+/**
+* Creating Product with i10n support ..label field value is defer for each language
+* Create Product Folder for Storing all the newly created products
+*/
+
+$productfolder = CSPms::createProductFolder('ProductFolder',null,CSITEM_POSITION_CHILD,0);
+$productfolderID = $productfolder->store();
+$productfolder->checkin();
+
+echo '<br/>the product folder is created with folder name '.$productfolder->getLabel().' and folder id '.$productfolderID;
+
+
+
+/**
+* Create one Product under the above defined Product Folder
+*/
+
+$product = CSPms::createProduct('Product1',$productfolderID);
+
+$productNameArray = array("Product1"=>1,"ProductDE"=>2,"ProductFR"=>3,"ProductSP"=>4);
+
+foreach($productNameArray as $productName => $langauge){
+ $product->setValue("Label", $productName,$langauge);
+}
+
+$productID = $product->store();
+$product->checkin();
+
+echo '<br/> the product is created with product name '.$product->getLabel().' and product id '.$productID;
+
+
+/**
+* Create Class for the given Product
+*/
+
+$class = CSPms::createClass('Class1',null,CSITEM_POSITION_CHILD);
+$classID = $class->store();
+
+echo '<br/>the configuration is created with name '.$class->getlabel().' and it configuration id '.$classID;
+
+
+/**
+* Create Attributes to assign to the Given Configuration
+*/
+
+$configuration = CSPms::createConfiguration('Configuration1',null,CSITEM_POSITION_CHILD);
+$configurationId = $configuration->store();
+
+
+$attributeID = array();
+
+ for($i=0; $i<2; $i++)
+ {
+	$attribute = CSPms::createField('Attribute_'.$i,$configurationId,CSITEM_POSITION_CHILD,'caption');
+	$attributeID[$i] = $attribute->store();
+	$class->addField($attribute->getID());
+	$class->store();
+ }
+ 
+ echo '<br/>the attributes is created with and stored inside the array' ;
+ 
+ 
+ /**
+* Assign the Class or Configuration to Product
+*/
+
+$product = CSPms::getProduct($productID);
+$product->checkout();
+$product->setBaseField($classID);
+$product->store();
+$product->checkin();
+
+echo '<br/> the product name '.$product->getLabel().' has configuration or class '.$class->getLabel();
+
+
+/**
+* Assign value for all the attribute of this class for this product
+*/
+
+/* Creating the Key Value Array for Testing setValues as per specified format.*/
+$keyValue = array();
+
+for($j=0; $j<sizeof($attributeID); $j++)
+{
+	$keyValue[$attributeID[$j]]= 'Value_'.$j;
+}
+
+
+$product = CSPms::getProduct($productID);
+$product->checkout();
+// Using setValues Instead of setValue 
+$product->setValues($keyValue);
+$product->store();
+$product->checkin();
+
+echo '<br/> the test executed sucessfully';
